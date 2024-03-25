e48eb8ac5114912fb2692dfd597a69bfcd1db30b
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2024-03-26 01:38:13 +0300

    feat: add fifth solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -102,8 +102,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const firstDate = new Date(dateStart);
+  const lastDate = new Date(dateEnd);
+
+  return Math.floor(new Date(lastDate - firstDate) / 1000 / 60 / 60 / 24) + 1;
 }
 
 /**
@@ -252,8 +255,10 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const currYear = date.getFullYear();
+  const leap = currYear % 4 === 0 && currYear % 100 !== 0;
+  return leap || currYear % 400 === 0;
 }
 
 module.exports = {
